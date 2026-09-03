37080720b3afbf3881448c57cb72a360782b1966
Akshay Shukla <6854.akshay@gmail.com>
2026-09-03 10:05:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (83.37%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+        string toLowerCase(string s)
+        {
+            string s1="";
+            for (auto i : s)
+            {
+                if (i >= 'A' and i <= 'Z')
+                    s1.push_back(i + 32);
+                else
+                    s1.push_back(i);
+            }
+            return s1;
+        }
+};
